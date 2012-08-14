353ea9a7cac0e142d734610879cd76fbe2444ca0
Raphael Costa <raphael@raphaelcosta.net>
2012-08-14 11:32:46 -0300

    new methods

--- lib/google_calendar_oauth2/calendar.rb
+++ lib/google_calendar_oauth2/calendar.rb
@@ -8,7 +8,16 @@ module GoogleCalendar
       list.data.items
     end
 
-    def self.find(query)
+    def self.find(calendarId)
+      connection.execute(client.calendars.get, {'calendarId' => calendarId}).data
+    end
+
+    def self.destroy(calendarId)
+      response = connection.execute(client.calendars.delete, {'calendarId' => calendarId})
+      response.status == 204
+    end
+
+    def self.find_by_name(query)
       list.each do |cal|
         if cal.summary == query
           return @cal = cal
@@ -20,5 +29,6 @@ module GoogleCalendar
     def self.create(attrs)
       connection.execute(api_method: client.calendars.insert, body: [JSON.dump(attrs)], headers: {'Content-Type' => 'application/json'})
     end
+
   end
 end
